d9b7d11ef488206eb8460d44ea98c39421aeaf35
Payton Swick <payton@foolord.com>
2016-07-31 20:45:51 -0400

    Add currentBranchName

--- crow.js
+++ crow.js
@@ -1,10 +1,24 @@
+import path from 'path';
+
 class Crow {
 	constructor( dir ) {
 		this.rootDir = dir;
+		this.currentBranchName = null;
 	}
 
 	getRootDir() {
 		return this.rootDir;
+	}
+
+	setCurrentBranch( name ) {
+		this.currentBranchName = name;
+	}
+
+	getBranchFile() {
+		if ( ! this.currentBranchName ) {
+			return null;
+		}
+		return path.join( this.getRootDir(), 'refs', 'heads', this.currentBranchName );
 	}
 }
 
--- test/crow.js
+++ test/crow.js
@@ -2,11 +2,30 @@ import { expect } from 'chai';
 
 import { getCrowAt } from '../crow';
 
-describe( 'getRootDir', function() {
-	const mockRootDir = '/foo/bar';
+const mockRootDir = '/foo/bar';
+const firstBranchName = 'my-branch';
 
-	it( 'returns the full path to the config directory', function() {
-		const crow = getCrowAt( mockRootDir );
-		expect( crow.getRootDir() ).to.eql( mockRootDir );
+describe( 'crow', function() {
+	let crow;
+
+	beforeEach( function() {
+		crow = getCrowAt( mockRootDir );
+	} );
+
+	describe( '.getRootDir', function() {
+		it( 'returns the full path to the config directory', function() {
+			expect( crow.getRootDir() ).to.eql( mockRootDir );
+		} );
+	} );
+
+	describe( '.getBranchFile', function() {
+		it( 'returns null when there is no branch set', function() {
+			expect( crow.getBranchFile() ).to.eql( null );
+		} );
+
+		it( 'returns the full path to the branch directory when there is a branch set', function() {
+			crow.setCurrentBranch( firstBranchName );
+			expect( crow.getBranchFile() ).to.eql( `${mockRootDir}/refs/heads/${firstBranchName}` );
+		} );
 	} );
 } );
